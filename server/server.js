feabd3c582272fbb9aaaf996ac6a731856cffa64
James Langridge <l@ngridge.com>
2025-08-18 13:06:36 +0200

    Fix missing crypto import causing 500 error on suggestions

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import IORedis from "ioredis";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import crypto from "crypto";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
